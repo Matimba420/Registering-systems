38b8baf47ba749e988ba4a3b399d163db864d57c
Shiba Koenaite <shiba.koenaite@dadevs.co.za>
2022-02-11 09:28:07 +0200

    Updated the backend & the db

---
+++ backend/routes/attendance.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const commentContlroller = require('../controllers/attendanceController');
+
+router.get('/', attendanceController.getAll)
+
+module.exports = router
