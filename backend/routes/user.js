6fc0fdab0ea1b46a688ec975ed0fbe82728bcbb5
Shiba Koenaite <shiba.koenaite@dadevs.co.za>
2022-02-14 09:30:39 +0200

    Updated the services folder

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,17 +6,17 @@ const router = express.Router();
 
 router.post('/register', userController.register)
 
-// // Sign In with emp_id and password
-// router.post('/login', userController.logIn)
+// Sign In with emp_id and password
+router.post('/login', userController.logIn)
 
-// /**
-//  * delete user by emp_id
-//  */
-// router.delete('/:id', userController.removeOne)
+/**
+ * delete user by emp_id
+ */
+router.delete('/:id', userController.removeOne)
 
-// /**
-//  * update a user by emp_id
-//  */
-// router.put('/:id', userController.update)
+/**
+ * update a user by emp_id
+ */
+router.put('/:id', userController.update)
 
 module.exports = router
