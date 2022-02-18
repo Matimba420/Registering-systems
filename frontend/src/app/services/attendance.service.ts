7d12d784c3ca0b67c43c9b17f7a28cfec5ef9767
ZiziphoMa <magugwanazizipho94@gmail.com>
2022-02-18 14:42:35 +0200

    Fixed Attendace.services

--- frontend/src/app/services/attendance.service.ts
+++ frontend/src/app/services/attendance.service.ts
@@ -12,12 +12,12 @@ export class AttendanceService {
   constructor(private http:HttpClient) { }
 
   getAllOneId(emp_id: String) {
-    return this.http.get('http://localhost:3100/attendance/getAllOneId/'+emp_id)
+    return this.http.get(`${environment.baseUrl}attendance/getAllOneId/`+emp_id)
   }
   getAll(): Observable<any>{
     return this.http.get<Employee[]>(`${environment.backend}/attendance/getAll`);
   }
   attend(employee : Employee) {
-    return this.http.post('http://localhost:3100/attendance/attend', employee);
+    return this.http.post(`${environment.backend}/attendance/attend`, employee);
   }
 }
